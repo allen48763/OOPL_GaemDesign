534376d93c0de533fe75c29cef7b24c1408f083c
asdf456753123 <97729097+asdf456753123@users.noreply.github.com>
2022-03-18 09:58:01 +0800

    練習 7 – 乙: Pull 並上傳新的程式碼

--- GitTutorial.cpp
+++ GitTutorial.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 
 bool Guess(int number) {
+    static int target = -1;
+    srand(time(NULL));
+    if(target == -1){
+
+        target=rand() % 100 +1;
+    }
+    if(number == target)
+        return true;
     return false;
 }
 
